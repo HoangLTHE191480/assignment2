d49fa2ed5b83649d072a03fd1dc07b8a32c30f53
hoang <hgle249305@gmail.com>
2026-01-28 20:49:53 +0700

    Base calculator code

---
+++ assignment2/assignment2/Calculator.cs
@@ -0,0 +1,20 @@
+﻿public class Calculator
+{
+    public enum SIGN
+    {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+
+    public double Calculate(SIGN sign, double arg1, double arg2)
+    {
+        switch (sign)
+        {
+            default:
+                break;
+        }
+        return 0d;
+    }
+}
