e9c9af50563d2c80b380e7b5d45d3883e640253f
hungntm <hungminhml26@gmail.com>
2026-01-29 09:06:35 +0700

    Code base

---
+++ assignment2/assignment2/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    enum SIGN
+    {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+
+    static double Calculate(SIGN sign, double arg1, double arg2)
+    {
+        switch (sign)
+        {
+            default:
+                return 0d;
+        }
+    }
+
+}
